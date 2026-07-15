d8162274ae533459f97b85d4dd9357332f289d59
Martin Gabriel Gardea <martingabrielgardea@gmail.com>
2026-07-15 12:48:07 +0200

    feat: add interactive command loop to main()

--- todolist.cpp
+++ todolist.cpp
@@ -1,11 +1,52 @@
 #include "todolist.hpp"
 
+void vypisNapovedu() {
+    std::cout << "\nPrikazy:\n"
+              << "  p <popis>  - pridat ukol\n"
+              << "  v          - vypsat ukoly\n"
+              << "  o <id>     - oznacit ukol jako hotovy\n"
+              << "  r <id>     - odebrat ukol\n"
+              << "  q          - konec\n"
+              << "> ";
+}
+
 int main() {
     const std::string soubor = "ukoly.txt";
-
     std::vector<Task> ukoly = nactiUkoly(soubor);
 
-    ulozUkoly(ukoly, soubor);
+    std::string radek;
+    while (true) {
+        vypisNapovedu();
+        if (!std::getline(std::cin, radek)) break;
 
+        Prikaz prikaz = rozeberPrikaz(radek);
+        if (prikaz.typ == TypPrikazu::Konec) break;
+
+        switch (prikaz.typ) {
+            case TypPrikazu::Vypsat:
+                vytiskniUkoly(ukoly);
+                break;
+            case TypPrikazu::Pridat:
+                pridatUkol(ukoly, prikaz.popis);
+                break;
+            case TypPrikazu::Oznacit:
+                if (!oznacitUkolDokonceny(ukoly, prikaz.id)) {
+                    std::cout << "Ukol s ID " << prikaz.id << " nenalezen.\n";
+                }
+                break;
+            case TypPrikazu::Odebrat:
+                if (!odebratUkol(ukoly, prikaz.id)) {
+                    std::cout << "Ukol s ID " << prikaz.id << " nenalezen.\n";
+                }
+                break;
+            case TypPrikazu::Konec:
+                break;
+            default:
+                std::cout << "Neznamy prikaz.\n";
+                break;
+        }
+    }
+
+    ulozUkoly(ukoly, soubor);
     return 0;
 }
